8a44ec0a5c1cdd7cc8433917e62c7423eb70bb68
git12nazneen <lipinazneen65@gmail.com>
2024-08-02 11:42:05 +0600

    added swal alert

--- src/hooks/UseAxiosPublice.jsx
+++ src/hooks/UseAxiosPublice.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosPublic = axios.create({
-    baseURL: 'https://server-two-sage-80.vercel.app'
+    baseURL: 'http://localhost:5000'
 })
 
 
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import useAuth from "./useAuth";
 
 
  const axiosSecure = axios.create({
-    baseURL: 'https://server-two-sage-80.vercel.app'
+    baseURL: 'http://localhost:5000'
 })
 
 const useAxiosSecure = () => {
